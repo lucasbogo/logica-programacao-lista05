4651a4507733585bac5064268fe11e1eebead45e
Lucas Bogo <76253752+lucasbogo@users.noreply.github.com>
2022-04-25 23:23:03 -0300

    first commit
    It works!

---
+++ lista5-01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) 
+{
+
+    int i;
+
+        printf("Numeros de 1 a 20\n");
+
+            for (i = 1; i <= 20; i++)
+
+                printf("\n%d", i);
+                printf("\n\n");
+
+        system("pause");
+
+    return(0);
+}
