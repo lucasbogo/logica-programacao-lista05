b5a5f25c81976b5d620c2a4889e8db1d4e065f3a
Lucas Bogo <76253752+lucasbogo@users.noreply.github.com>
2022-04-25 23:28:26 -0300

    first commit
    It works

---
+++ lista5-02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (void)
+{
+    int i;
+
+        printf("15 a 1 em ordem decrescente\n");
+
+            for (i = 15; i >= 1; i--)
+
+            printf("\n%d", i);
+            printf("\n");
+
+        system("pause");
+
+    return(0);
+}
